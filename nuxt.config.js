fa1f7fd3975d0270a9662c4dd757ebcf998e8545
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-03-27 17:53:45 -0300

    add: nuxt.config.js

---
+++ nuxt.config.js
@@ -0,0 +1,116 @@
+export default {
+  // https://nuxtjs.org/deployments/netlify/
+  target: "static",
+
+  ssr: false,
+
+  generate: {
+    fallback: true,
+  },
+
+  // Headers of the page
+  head: {
+    title: "SonarFlix - Fast-track your entertainment picks.",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Browse Movies, TV Shows and People",
+      },
+      { hid: "author", name: "author", content: "ivanluna.dev" },
+      { hid: "og:locale", property: "og:locale", content: "en_GB" },
+      { hid: "og:title", property: "og:title", content: "Movies App" },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "Browse Movies, TV Shows and People",
+      },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://ivanluna.dev/",
+      },
+
+    ],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "//fonts.googleapis.com/css?family=Roboto:300,400,500",
+      },
+    ],
+  },
+
+  // Global CSS
+  css: ["@/assets/css/global.scss"],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    "~/plugins/lazyload.js",
+    "~/plugins/filters.js",
+    { src: "~/plugins/ga.js", ssr: false },
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    "@nuxtjs/eslint-module",
+  ],
+
+  modules: ["@nuxtjs/dotenv", "@nuxtjs/axios", "@nuxtjs/pwa"],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: "/",
+  },
+
+
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: "en",
+      name: "SonarFlix",
+      short_name: "SonarFlix",
+      description: "Fast-track your entertainment picks.",
+      theme_color: "#03496B",
+
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  loaders: {
+    cssModules: {
+      camelCase: true,
+      localIdentName: "[local]_[hash:base64:5]",
+    },
+  },
+
+  vue: {
+    config: {
+      productionTip: false,
+      devtools: false,
+    },
+  },
+
+  env: {
+    FRONTEND_URL: process.env.FRONTEND_URL || "",
+    API_KEY: process.env.API_KEY || "",
+    API_LANG: process.env.API_LANG || "en-US",
+    API_COUNTRY: process.env.API_COUNTRY || "GB",
+    API_YOUTUBE_KEY: process.env.API_YOUTUBE_KEY || "",
+    GA: process.env.GA || "",
+  },
+
+  // Customize the progress bar color
+  loading: {
+    color: "#2196f3",
+  },
+};
