0290406ae7bb24c6feac9fb47f7454d66fb8aa71
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-04-12 00:40:20 -0300

    refactor: remove console.log statements

--- api/index.js
+++ api/index.js
@@ -244,12 +244,9 @@ export function getMovie(id) {
       },
     }).then(async (response) => {
       const responseData = response.data;
-      console.log("Original Movie Data:", responseData); 
-
       try {
         const providers = await getMovieProviders(id);
         responseData.providers = providers;
-        console.log("Providers:", providers);
       } catch (error) {
         console.error("Error fetching movie providers:", error);
       }
@@ -267,7 +264,6 @@ export function getMovie(id) {
         imdb_id: responseData.external_ids ? responseData.external_ids.imdb_id : null,
         vote_average: responseData.vote_average,
       };
-      console.log("Movie Data:", JSON.stringify(movieData));
       resolve(responseData); 
     }).catch((error) => {
       console.error("Error fetching movie data:", error);
@@ -286,17 +282,13 @@ export function getMovieProviders(id) {
       let providers = response.data.results.AR; // Intentamos obtener primero los proveedores de AR
       if (providers && providers.flatrate) {
         const providerNames = providers.flatrate.map(provider => provider.provider_name);
-        console.log("Flatrate Providers in AR:", providerNames);
         resolve(providerNames);
       } else {
-        console.log("No flatrate providers found for AR, trying US");
         providers = response.data.results.US; // Intentamos obtener los proveedores de US
         if (providers && providers.flatrate) {
           const providerNames = providers.flatrate.map(provider => provider.provider_name);
-          console.log("Flatrate Providers in US:", providerNames);
           resolve(providerNames);
         } else {
-          console.log("No flatrate providers found for US, unable to fetch movie providers");
           reject(new Error("Unable to fetch movie providers"));
         }
       }
@@ -317,17 +309,13 @@ export function getTVShowProviders(id) {
       let providers = response.data.results.AR; // Intentamos obtener primero los proveedores de AR
       if (providers && providers.flatrate) {
         const providerNames = providers.flatrate.map(provider => provider.provider_name);
-        console.log("Flatrate Providers in AR:", providerNames);
         resolve(providerNames);
       } else {
-        console.log("No flatrate providers found for AR, trying US");
         providers = response.data.results.US; // Intentamos obtener los proveedores de US
         if (providers && providers.flatrate) {
           const providerNames = providers.flatrate.map(provider => provider.provider_name);
-          console.log("Flatrate Providers in US:", providerNames);
           resolve(providerNames);
         } else {
-          console.log("No flatrate providers found for US, unable to fetch TV show providers");
           reject(new Error("Unable to fetch TV show providers"));
         }
       }
@@ -383,12 +371,9 @@ export function getTvShow(id) {
       },
     }).then(async (response) => {
       const responseData = response.data;
-      console.log("Original TV Show DATA:", responseData)
-
       try {
         const providers = await getTVShowProviders(id);
         responseData.providers = providers;
-        console.log("Providers:", providers);
       } catch (error) {
         console.error("Error fetching movie providers:", error);
       }
@@ -406,7 +391,6 @@ export function getTvShow(id) {
         imdb_id: responseData.external_ids ? responseData.external_ids.imdb_id : null,
         vote_average: responseData.vote_average,
       };
-      console.log("TV Show Data:", JSON.stringify(tvShowData));
       resolve(responseData); 
     }).catch((error) => {
       console.error("Error fetching TV show data:", error);
