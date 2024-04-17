b1a992576b68556f7946a3aee36f366062ef4533
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-04-16 22:29:56 -0300

    chore: enhance variable naming and introduce card__label property for responsive card titles on screens narrower than 1200px

--- components/CustomListingCategoriesSeries.vue
+++ components/CustomListingCategoriesSeries.vue
@@ -20,12 +20,12 @@
   
         <div ref="carouselElement" class="carousel__items" @scroll="scrollEvent">
           <div v-for="(category, index) in categories" :key="`category-${index}`" class="card" @click="redirectToRoute(category.link)">
-            <div class="card__img2">
+            <div class="card__image">
               <img :src="category.image" :alt="category.alt">
             </div>
   
             <div class="card__info">
-              <h2 class="card__name">
+              <h2 class="card__label">
                 <a @click.prevent="redirectToRoute(category.link)" class="category-link">{{ category.alt }}</a>
               </h2>
             </div>
@@ -69,7 +69,7 @@
     data() {
       return {
         categories: [
-        { image: "/thumbnails/talkshow-tv-esp.png", link: "/genre/10767/tv", alt: "Programas de Televisión" },
+        { image: "/thumbnails/talkshow-tv-esp.png", link: "/genre/10767/tv", alt: "Programas de TV" },
         { image: "/thumbnails/comedy-tv-esp.png", link: "/genre/35/tv", alt: "Comedia" },
         { image: "/thumbnails/mistery-tv-esp.png", link: "/genre/9648/tv", alt: "Misterio" },
         { image: "/thumbnails/sciencefiction-movie-esp.png", link: "/genre/10765/tv", alt: "Ciencia Ficción y Fantasía" },
@@ -137,7 +137,7 @@
     margin-left: 1rem;
   }
   
-  .card__img2 {
+  .card__image {
     border-radius: 15px;
     cursor: pointer;
   }
