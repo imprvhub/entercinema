66e3391dccbb39b6950f092fdbb2f9f8b3d7cb28
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-04-23 09:20:23 -0300

    chore: enhance style of elements in footer

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -17,7 +17,7 @@
     <br>
     <p>All Data is provided by 
         <a target="_blank" href="https://www.themoviedb.org/" rel="noopener">
-            <img src="~static/tmdb.svg" alt="TMDB Attribution" class="tmdb-attribution" style="width: 60px; height: 35px; display: inline-block; margin-bottom: 1px;">
+            <img src="~static/tmdb.svg" alt="TMDB Attribution" class="tmdb-attribution" style="width: 60px; height: 35px; display: inline-block; margin-bottom: 2px;">
         </a>
         and 
         <a target="_blank" href="https://www.justwatch.com/" rel="noopener" style="margin-top: 2px;">JustWatch</a>.
