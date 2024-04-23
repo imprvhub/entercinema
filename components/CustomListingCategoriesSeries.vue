539c99b94aac8326c233b0213e1fe922db58c8d5
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-04-23 12:17:17 -0300

    fix: typo

--- components/CustomListingCategoriesSeries.vue
+++ components/CustomListingCategoriesSeries.vue
@@ -2,7 +2,7 @@
     <div class="listing-carousel-categories">
       <br>
       <div v-if="title || viewAllUrl" class="listing__head">
-        <h2 class="listing__title">Categories: Tv Shows</h2>
+        <h2 class="listing__title">Categories: TV Shows</h2>
         <nuxt-link v-if="viewAllUrl" :to="viewAllUrl" class="listing__explore">
           <strong>Explore All</strong>
         </nuxt-link>
