092c1725a2e7c8e20573129c7e10fc8671ae8beb
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-05-22 11:39:28 -0300

    fix: typo

--- components/Hero.vue
+++ components/Hero.vue
@@ -174,7 +174,7 @@
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
                               <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                           </svg>
-                          <span style="margin-left: 0.1rem;">Twitter</span>
+                          <span style="margin-left: 0.1rem;">Twitter/X</span>
                       </button>
                       <button @click="shareTo('email')" style="background: #082D3E; color: #fff; border: none; padding: 0.5rem 1rem; margin-bottom: 1rem; border-radius: 0.5rem; cursor: pointer; transition: background 0.3s ease; font-size: 1.1rem;">
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
