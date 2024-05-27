ec387757026cc216a6efc275e1e49627657a8525
Iván Luna <133716241+imprvhub@users.noreply.github.com>
2024-05-27 02:52:12 -0300

    fix: added loader functionality to sign in button and updated layout for login, register, and recovery pages to include tab navigation.

--- pages/recovery/index.vue
+++ pages/recovery/index.vue
@@ -1,40 +1,42 @@
 <template>
   <main class="main">
-    <section class="section">
-      <br>
-      <h1 class="text-white text-center"><b>Reset Your Password</b></h1>
-      <h3 class="text-white text-center" v-if="!confirmationMessage"><b>Type in your email and we'll send you a link to reset your password:</b></h3>
-      <h3 class="text-white text-center" v-else><b> </b></h3>
-      <br>
-      <div class="form">
-        <div v-if="confirmationMessage" class="confirmation-message">
-          <p style="text-align: center; width:200px">An email has been sent to {{ email }}. Please check your inbox and reset your password.</p>
-          <p style="text-align: center; width:200px">If you encounter any issues or have any questions, please contact to <a href="mailto:contact@ivanluna.dev">contact@ivanluna.dev</a>.</p>
-          <div class="button-container">
-            <button class="button button--icon" @click="redirectToHome">
+    <div class="auth-container" style="padding-top:20px;">
+      <div class="container-section">
+        <br>
+        <h1 class="text-white text-center"><b>Reset Your Password</b></h1>
+        <h3 class="text-white text-center" v-if="!confirmationMessage"><b>Type in your email and we'll send you a link to reset your password:</b></h3>
+        <h3 class="text-white text-center" v-else><b> </b></h3>
+        <br>
+        <div class="form">
+          <div v-if="confirmationMessage" class="confirmation-message">
+            <p style="text-align: center; width:200px">An email has been sent to {{ email }}. Please check your inbox and reset your password.</p>
+            <p style="text-align: center; width:200px">If you encounter any issues or have any questions, please contact to <a href="mailto:contact@ivanluna.dev">contact@ivanluna.dev</a>.</p>
+            <div class="button-container">
+              <button class="button button--icon" @click="redirectToHome">
+                  <span class="txt">Back</span>
+              </button>
+            </div>
+          </div>
+          <form v-else @submit.prevent="recoverPassword">
+            <div v-if="!confirmationMessage" class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" id="email" v-model="email" placeholder="johndoe@example.com" required>
+            </div>
+            <br>
+            <div class="button-container">
+              <button class="button button--icon" @click="redirectToHome">
                 <span class="txt">Back</span>
-            </button>
-          </div>
+              </button>
+              <button v-if="!confirmationMessage" type="submit" class="button button--icon">
+                <span class="txt">Reset Password</span>
+              </button>
+            </div>
+          </form>
         </div>
-        <form v-else @submit.prevent="recoverPassword">
-          <div v-if="!confirmationMessage" class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" v-model="email" placeholder="johndoe@example.com" required>
-          </div>
-          <br>
-          <div class="button-container">
-            <button class="button button--icon" @click="redirectToHome">
-              <span class="txt">Back</span>
-            </button>
-            <button v-if="!confirmationMessage" type="submit" class="button button--icon">
-              <span class="txt">Reset Password</span>
-            </button>
-          </div>
-        </form>
+        <br>
+        <h2 class="text-center custom-center"><strong>Already have an account? <router-link :to="{ name: 'login' }">Sign In</router-link></strong></h2>
       </div>
-      <br>
-      <h2 class="text-center custom-center"><strong>Already have an account? <router-link :to="{ name: 'login' }">Sign In</router-link></strong></h2>
-    </section>
+    </div>
   </main>
 </template>
 
@@ -60,6 +62,31 @@ export default {
 
 
 <style scoped>
+
+.auth-section {
+  background: rgba(6, 47, 64, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 97, 135, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  padding: 20px;
+  margin: 20px;
+}
+
+.container-section {
+  background: rgba(6, 47, 64, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 97, 135, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  padding: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
 a {
   font-weight: 600;
   color: #80868b;
--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,62 +1,69 @@
 <template>
   <main class="main">
-    <section class="section">
-      <br>
-      <h1 class="text-white text-center"><b>Get started</b></h1>
-      <div v-if="!showVerificationModal">
-        <h3 class="text-white text-center"><b>Create a new account:</b></h3>
-        <div class="form">
-          <form @submit.prevent="register">
-            <div class="form-group">
-              <label for="name">Name:</label>
-              <input type="text" id="name" v-model="name" placeholder="John Doe" required>
-            </div>
-            <br>
-            <div class="form-group">
-              <label for="email">Email:</label>
-              <input type="email" id="email" v-model="email" placeholder="johndoe@example.com" required>
-            </div>
-            <br>
-            <div class="form-group">
-              <label for="password">Password:</label>
-              <input type="password" id="password" v-model="password" placeholder="Enter your password" required @input="checkPassword">
-              <br>
-              <div class="password-requirements">
-                <ul>
-                  <label class="pass-req-label">Password requirements:</label>
+    <section class="auth-section">
+      <div class="tabs">
+        <span class="tab" @click="goToSignIn">Sign In</span>
+        <span class="tab active">Register</span>
+      </div>
+      <div class="auth-container">
+        <div class="container-section">
+          <h1 class="text-white text-center"><b>Get started</b></h1>
+            <div v-if="!showVerificationModal">
+              <h3 class="text-white text-center"><b>Create a new account:</b></h3>
+              <div class="form">
+                <form @submit.prevent="register">
+                  <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" v-model="name" placeholder="John Doe" required>
+                  </div>
                   <br>
-                  <li :class="{ 'text-success': hasUpperCase }"><span v-if="hasUpperCase">&#10003;</span><span v-else>&#9898;</span> One uppercase letter</li>
-                  <li :class="{ 'text-success': hasLowerCase }"><span v-if="hasLowerCase">&#10003;</span><span v-else>&#9898;</span> One lowercase letter</li>
-                  <li :class="{ 'text-success': hasNumber }"><span v-if="hasNumber">&#10003;</span><span v-else>&#9898;</span> One number</li>
-                  <li :class="{ 'text-success': hasSymbol }"><span v-if="hasSymbol">&#10003;</span><span v-else>&#9898;</span> One symbol</li>
-                  <li :class="{ 'text-success': hasMinLength }"><span v-if="hasMinLength">&#10003;</span><span v-else>&#9898;</span> Minimum 8 characters</li>
-                </ul>
+                  <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" v-model="email" placeholder="johndoe@example.com" required>
+                  </div>
+                  <br>
+                  <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" v-model="password" placeholder="Enter your password" required @input="checkPassword">
+                    <br>
+                    <div class="password-requirements">
+                      <ul>
+                        <label class="pass-req-label">Password requirements:</label>
+                        <br>
+                        <li :class="{ 'text-success': hasMinLength }"><span v-if="hasMinLength">&#10003;</span><span v-else>&#9898;</span> Minimum 8 characters</li>
+                        <li :class="{ 'text-success': hasUpperCase }"><span v-if="hasUpperCase">&#10003;</span><span v-else>&#9898;</span> One uppercase letter</li>
+                        <li :class="{ 'text-success': hasLowerCase }"><span v-if="hasLowerCase">&#10003;</span><span v-else>&#9898;</span> One lowercase letter</li>
+                        <li :class="{ 'text-success': hasNumber }"><span v-if="hasNumber">&#10003;</span><span v-else>&#9898;</span> One number</li>
+                        <li :class="{ 'text-success': hasSymbol }"><span v-if="hasSymbol">&#10003;</span><span v-else>&#9898;</span> One symbol</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div class="error-message" v-if="errorMessage" :style="{ height: errorMessage ? 'auto' : '0', margin: errorMessage ? '10px auto' : '0 auto', overflow: errorMessage ? 'visible' : 'hidden' }">
+                    <p>{{ errorMessage }}</p>
+                  </div>
+                  <div class="button-container">
+                    <button class="button button--icon" @click="redirectToHome">
+                      <span class="txt">Back</span>
+                    </button>
+                    <button type="submit" class="button button--icon" :disabled="!isFormValid || !isPasswordValid">
+                      <span class="txt">Sign Up</span>
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
-            <div class="error-message" v-if="errorMessage" :style="{ height: errorMessage ? 'auto' : '0', margin: errorMessage ? '10px auto' : '0 auto', overflow: errorMessage ? 'visible' : 'hidden' }">
-              <p>{{ errorMessage }}</p>
+            <div v-if="showVerificationModal" class="text-center custom-center" style="max-width: 250px; text-align: center; margin: 0 auto;">
+              <p class="text-center custom-center">Thank you, {{ name }}, for registering! Your account with the email {{ email }} has been created. Enjoy your Cinemathe experience!</p>
             </div>
-            <div class="button-container">
+            <div v-if="showVerificationModal" class="button-container">
               <button class="button button--icon" @click="redirectToHome">
                 <span class="txt">Back</span>
               </button>
-              <button type="submit" class="button button--icon" :disabled="!isFormValid || !isPasswordValid">
-                <span class="txt">Sign Up</span>
-              </button>
             </div>
-          </form>
-        </div>
+            <br>
+            <h3 class="text-center custom-center"><strong>Have an account? <router-link :to="{ name: 'login' }">Sign In Now</router-link></strong></h3>
       </div>
-      <div v-if="showVerificationModal" class="text-center custom-center" style="max-width: 250px; text-align: center; margin: 0 auto;">
-        <p class="text-center custom-center">Thank you, {{ name }}, for registering! Your account with the email {{ email }} has been created. Enjoy your Cinemathe experience!</p>
       </div>
-      <div v-if="showVerificationModal" class="button-container">
-        <button class="button button--icon" @click="redirectToHome">
-          <span class="txt">Back</span>
-        </button>
-      </div>
-      <br>
-      <h3 class="text-center custom-center"><strong>Have an account? <router-link :to="{ name: 'login' }">Sign In Now</router-link></strong></h3>
     </section>
   </main>
 </template>
@@ -95,11 +102,11 @@ export default {
           email: this.email,
           password: this.password
         });
-        this.showVerificationModal = true; 
+        this.showVerificationModal = true;
       } catch (error) {
         console.error(error);
         if (error.response && error.response.status === 500) {
-          this.errorMessage = 'Failed to sign up: A user with this email already exists.'; 
+          this.errorMessage = 'Failed to sign up: A user with this email already exists.';
         } else {
           this.errorMessage = 'An error occurred. Please try again later.';
         }
@@ -110,16 +117,86 @@ export default {
     },
     checkPassword() {
       this.hasUpperCase = /[A-Z]/.test(this.password);
-      this.hasLowerCase = /[a-z]+/.test(this.password); 
+      this.hasLowerCase = /[a-z]+/.test(this.password);
       this.hasNumber = /\d/.test(this.password);
       this.hasSymbol = /[!@#$%^&*()_+]/.test(this.password);
       this.hasMinLength = this.password.length >= 8;
-    }
+    },
+    goToSignIn() {
+      this.$router.push({ name: 'login' });
+    },
   }
 };
 </script>
 
 <style scoped>
+.auth-section {
+  background: rgba(6, 47, 64, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 97, 135, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  padding: 20px;
+  margin: 20px;
+}
+
+.container-section {
+  background: rgba(6, 47, 64, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 97, 135, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  padding: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.tab {
+  padding: 10px 20px;
+  cursor: pointer;
+  color: #80868b;
+  background-color: transparent;
+  border: 1px solid transparent;
+  border-bottom: none;
+}
+
+.tab.active {
+  color: #ffffff;
+  background: rgba(10, 77, 106, 0.729);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 5px;
+  margin-left: 5px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  border-bottom: none;
+}
+
+.tab:not(.active) {
+  color: #ffffff;
+  background: rgba(11, 75, 103, 0.15);
+  backdrop-filter: blur(13.5px);
+  margin-right: 5px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 5px;
+}
+
+.text-center {
+  text-align: center;
+}
+
 .error-message {
     color: red;
     text-align: center;
@@ -251,4 +328,65 @@ h3 {
     margin-left: -20px; 
 }
 
+@media (max-width: 400px) {
+  .button {
+    width: 80px;
+    height: 40px;
+    font-size: 9px;
+  }
+
+  .form-group {
+    margin-left: 13rem;
+  }
+
+  .password-requirements {
+    margin-left: 4rem;
+  }
+
+  .form-group input[type="email"] {
+  margin-left: 20px;
+  max-width: 100px;
+}
+.form-group input[type="password"] {
+  margin-left: -1px;
+  max-width: 100px;
+}
+
+.form-group input[type="text"] {
+  margin-left: 20px;
+  max-width: 100px;
+}
+
+.tabs {
+  width: 5%;
+  height: 3%;
+  justify-content: unset;
+  left: 5%;
+  margin-left: 3rem;
+}
+
+.tab.active {
+  max-width: 105px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
+  flex-direction: column-reverse;      
+}
+
+.tab:not(.active) {
+  flex-wrap: wrap;
+  max-width: 105px;
+  margin: 0 auto;
+  text-align: center;
+  display:flex; 
+  justify-content: center;
+}
+
+.spinner {
+  left: 3px;
+  padding: 2px;
+  margin-right: 3px;
+}
+}
 </style>
