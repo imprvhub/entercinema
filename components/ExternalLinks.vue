7e72abcd8489d03c6fce920b36e654a97c4fa10c
imprvhub <ivanflowtramites@gmail.com>
2025-10-26 05:33:57 -0300

    refactor(external-links): rearrange links by priority and relevance
    Reorganized external links to prioritize ratings and community platforms before streaming options and social media. IMDb moved to top as it's already featured in the hero section.

--- components/ExternalLinks.vue
+++ components/ExternalLinks.vue
@@ -2,61 +2,6 @@
   <div class="external-links-container">
     <h4 class="section-title">Links Externos</h4>
     <div class="links-grid">
-      <div v-if="tomatoMeter.found && (currentPageType === 'movie' || currentPageType === 'tv')" class="link-item">
-        <a
-          :href="tomatoMeter.url"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Rotten Tomatoes"
-        >
-          <img
-            src="/rotten-tomatoes.svg"
-            alt="YTS"
-            class="link-icon rotten-tomatoes"
-            width="23"
-            height="23"
-          />
-          <span class="label-style">{{ tomatoMeter.score }}% Tomatómetro</span>
-        </a>
-      </div>
-      <div v-if="ytsUrl && currentPageType === 'movie'" class="link-item">
-        <a
-          :href="ytsUrl"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Visit YTS"
-        >
-          <img
-            src="/yts-logo.svg"
-            alt="YTS"
-            class="link-icon yts-logo"
-            width="23"
-            height="23"
-          />
-          <span class="label-style">Ver en YTS</span>
-        </a>
-      </div>
-      <div v-if="links.imdb_id && (currentPageType === 'movie' || currentPageType === 'tv')" class="link-item">
-        <a
-          :href="stremioLink"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Watch on Stremio"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 192 192"
-            fill="currentColor"
-            class="link-icon stremio-icon"
-          >
-            <path d="M100.242 23.743a6 6 0 0 0-8.485 0L23.743 91.757a6 6 0 0 0 0 8.485l68.014 68.015a6 6 0 0 0 8.485 0l68.015-68.015a6 6 0 0 0 0-8.485l-68.015-68.014ZM80 71.83c0-2.44 2.758-3.86 4.744-2.442l33.838 24.17a3 3 0 0 1 0 4.883l-33.838 24.171C82.758 124.03 80 122.611 80 120.17V71.83Z"/>
-          </svg>
-          <span class="label-style">Ver en Stremio</span>
-        </a>
-      </div>
-
       <div v-if="links.imdb_id" class="link-item">
         <a
           :href="`https://www.imdb.com/${imdb}/${links.imdb_id}`"
@@ -111,6 +56,23 @@
           <span class="label-style">Letterboxd</span>
         </a>
       </div>
+      <div v-if="tomatoMeter.found && (currentPageType === 'movie' || currentPageType === 'tv')" class="link-item">
+        <a
+          :href="tomatoMeter.url"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Rotten Tomatoes"
+        >
+          <img
+            src="/rotten-tomatoes.svg"
+            alt="YTS"
+            class="link-icon rotten-tomatoes"
+            width="23"
+            height="23"
+          />
+          <span class="label-style">{{ tomatoMeter.score }}% Tomatómetro</span>
+        </a>
+      </div>
 
       <div v-if="links.imdb_id" class="link-item">
         <a
@@ -143,6 +105,45 @@
         </a>
       </div>
       
+      <div v-if="links.imdb_id && (currentPageType === 'movie' || currentPageType === 'tv')" class="link-item">
+        <a
+          :href="stremioLink"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Watch on Stremio"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 192 192"
+            fill="currentColor"
+            class="link-icon stremio-icon"
+          >
+            <path d="M100.242 23.743a6 6 0 0 0-8.485 0L23.743 91.757a6 6 0 0 0 0 8.485l68.014 68.015a6 6 0 0 0 8.485 0l68.015-68.015a6 6 0 0 0 0-8.485l-68.015-68.014ZM80 71.83c0-2.44 2.758-3.86 4.744-2.442l33.838 24.17a3 3 0 0 1 0 4.883l-33.838 24.171C82.758 124.03 80 122.611 80 120.17V71.83Z"/>
+          </svg>
+          <span class="label-style">Ver en Stremio</span>
+        </a>
+      </div>
+
+      <div v-if="ytsUrl && currentPageType === 'movie'" class="link-item">
+        <a
+          :href="ytsUrl"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit YTS"
+        >
+          <img
+            src="/yts-logo.svg"
+            alt="YTS"
+            class="link-icon yts-logo"
+            width="23"
+            height="23"
+          />
+          <span class="label-style">Ver en YTS</span>
+        </a>
+      </div>
+
       <div v-if="links.twitter_id" class="link-item">
         <a
           :href="`https://twitter.com/${links.twitter_id}`"
