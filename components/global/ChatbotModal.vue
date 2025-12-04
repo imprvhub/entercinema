a85220885468784631d2a687a484d99922ecfd57
imprvhub <ivanflowtramites@gmail.com>
2025-12-04 15:00:15 -0300

    fix: Adjust chatbot modal overlay height.

--- components/global/ChatbotModal.vue
+++ components/global/ChatbotModal.vue
@@ -2773,7 +2773,7 @@ export default {
   top: 0;
   left: 0;
   width: 100vw;
-  height: 100dvh;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.75);
   backdrop-filter: blur(5px);
   display: flex;
