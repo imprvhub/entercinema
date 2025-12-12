c0142c4a99dc700834d3300698744d3503413982
imprvhub <ivanflowtramites@gmail.com>
2025-12-12 20:23:54 -0300

    refactor(ui): Replace assets.

--- pages/watchlist/index.vue
+++ pages/watchlist/index.vue
@@ -2281,7 +2281,7 @@ export default {
     font-size: 1.6rem;
     font-weight: 500;
     text-align: center;
-    font-family: 'GrandCru', 'Roboto', sans-serif;
+    font-family: 'Ortica', 'Roboto', sans-serif;
     font-weight: 300;
     letter-spacing: 0.05em;
     line-height: 1.2;
@@ -2325,7 +2325,7 @@ export default {
     transition: all 0.2s ease;
     position: relative;
     text-align: center;
-    font-family: 'GrandCru', 'Roboto', sans-serif;
+    font-family: 'Ortica', 'Roboto', sans-serif;
     font-weight: 300;
     letter-spacing: 0.05em;
     line-height: 1.2;
