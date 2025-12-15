3f7423bf0c00a3b80050ced04ef29b5adf9e0270
imprvhub <ivanflowtramites@gmail.com>
2025-12-15 16:56:18 -0300

    fix: typo.

--- pages/notifications/index.vue
+++ pages/notifications/index.vue
@@ -9,7 +9,7 @@
           </div>
 
           <p class="title-secondary">
-            Mantente al día sobre el nuevo contenido de las personas y series que sigues.<button @click="openHowItWorksModal" class="help-icon-button"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="#8BE9FD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10" /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><path d="M12 17h.01" /></svg></button>
+            Mantente al día sobre el nuevo contenido de las personas, productoras y series que sigues.<button @click="openHowItWorksModal" class="help-icon-button"><svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none" stroke="#8BE9FD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10" /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><path d="M12 17h.01" /></svg></button>
           </p>
           <div class="header-actions">
             <button 
