730ae0b365853bb8aeef181de47df1201c715ed5
imprvhub <ivanflowtramites@gmail.com>
2025-12-19 19:03:22 -0300

    fix: typo

--- pages/notifications/index.vue
+++ pages/notifications/index.vue
@@ -469,7 +469,7 @@ export default {
       }
 
       if (notification.media_type === 'movie' || notification.media_type === 'tv') {
-        return '/image_not_found_yet.webp';
+        return '/image_not_found_yet_es.webp';
       }
       
       if (notification.media_type === 'episode' && notification.person_id) {
